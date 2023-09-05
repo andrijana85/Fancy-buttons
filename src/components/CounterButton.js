8a3913bb8a1033c1dad7b1f2b6ab51a657bc0a19
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-05 00:00:41 -0400

    create components

---
+++ src/components/CounterButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CounterButton(props) {
+  return (
+    <button className="CounterButton">
+    You clicked me X amount of times
+    </button>
+  );
+} 
+
+export default CounterButton;
