3c4e4e51edacddebdfb484566e709b5380f0f691
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-05 22:43:17 -0400

    refactoringour state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import LightSwitchButton from './components/LightSwitchButton';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import TextRepeaterButton from './components/TextRepeaterButton'; 
+import { useState } from 'react';
 
 
 function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off' ? 'dark' : '');
+  
   return (
-    <div className="App">
+    <div className={`App ${dark}`}>
       <h1><span>Fancy Buttons!</span></h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
         <TextRepeaterButton />
       </section>
     </div>
