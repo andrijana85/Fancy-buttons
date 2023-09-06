5be754de650e4126508e01a73d732651427358c6
andrijana85 <topalovic.andrijana@yahoo.com>
2023-09-05 23:35:30 -0400

    Refactoring with useReducer

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { useState } from "react";
+import { useReducer } from "react";
 
 function AngryButton(props) {
-  const [anger, setAnger] = useState(0);
-  const handleClick = (amount) => {
-    if (anger < 1) {
-      setAnger(anger + amount);
-    } else {
-      setAnger(0);
-    }
-  }
+
+
+  const [anger, dispatch] = useReducer((anger, action) => {
+    // if (anger < 1) {
+    //  return anger + action;
+    // } else {
+    //   return 0;
+    // }
+    return anger > 1 ? 0 : anger + action;
+  }, 0);
+
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={() => handleClick(0.1)} className="AngryButton">
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={() => dispatch(0.1)} className="AngryButton">
     {/* When the threshold is not reached */}
     {anger < 1 && <span>Don't click me too much! </span>}
     {/* When the threshold is not reached */}
